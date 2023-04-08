918b4e528cda937112f730ad4f2853d0a651292c
Botond Ortutay <boortu@utu.fi>
2023-04-08 20:46:44 +0300

    new command data structure (2d array of commands), added to main.cpp

--- app/main.cpp
+++ app/main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 #include <string>
+#include "metaheaders/commstruct.h"
 #include "modules/main/command_parser.h"
 
 int main() {
 	std::string uInp;
 	std::string commOutp;
 
+	// 2d array where all command data is stored
+	// Dimensions: support for 100 modules with 100 commands each
+	command commDataStructure[100][100];
+
+	// Program execution starts here
 	std::cout << "graph_alg_test_env v0.1.0.2 by Botond Ortutay \n";
 	std::cout << "Please enter a command! \n";
 
