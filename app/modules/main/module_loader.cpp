bbe7c9f43171f162dd2205a3bbb1f7aa050f3dd8
Botond Ortutay <boortu@utu.fi>
2023-04-11 15:39:22 +0300

    progress with module loader

--- app/modules/main/module_loader.cpp
+++ app/modules/main/module_loader.cpp
@@ -4,26 +4,56 @@
 #include <sstream>
 #include "../../metaheaders/commstruct.h"
 
-void loadModule(std::string module){
+void loadModule(std::string module, command commDS[100][100]){
 	// File input
 	std::string mPath;
 	mPath = "app/modules/" + module + "/commands.csv";
 	std::ifstream filein(mPath);
+	command tempComm;
 
 	// File parsing
 	int pos;
+	int commSHelper; // 0 for command 1 for id 2 for desc
+	int lineno = 0;
 	for (std::string line; std::getline(filein, line);){
 		pos = 0;
+		commSHelper = 0;
 		for (int i = 0; i <= line.size(); i++){
 			if (line[i] == ',' || i == line.size()){
-				std::cout << line.substr(pos, i-pos) << "\n";
+				switch(commSHelper){
+					case 0:
+						tempComm.command =  line.substr(pos, i-pos);
+						break;
+					case 1:
+						tempComm.id = stoi(line.substr(pos, i-pos));
+						break;
+					case 2:
+						tempComm.desc =  line.substr(pos, i-pos);
+						break;
+				}
 				pos = i+1;
+				commSHelper++;
 			}
 		}
+		commDS[0][lineno] = tempComm;
+		lineno++;
 	}
 }
 
 int main(){
-	loadModule("main");
+	// 2d array where all command data is stored
+	// Dimensions: support for 100 modules with 100 commands each
+	command commDataStructure[100][100];
+
+	// Filling unused module slot beginnings with "NULL" values
+	for(int i = 0; i < 100; i++){
+		commDataStructure[i][0] = {"",0,""};
+	}
+
+	loadModule("main", commDataStructure);
+
+	std::cout << commDataStructure[0][0].command << "\n";
+	std::cout << commDataStructure[0][1].command << "\n";
+
 	return 0;
 }
