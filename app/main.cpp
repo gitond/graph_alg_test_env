49ceb2808dc51a3305b4e74bfc5c3b74f05e4989
Botond Ortutay <boortu@utu.fi>
2023-04-15 01:06:28 +0300

    graph class plan created

--- app/main.cpp
+++ app/main.cpp
@@ -18,7 +18,7 @@ int main() {
 	}
 
 	// Program execution starts here
-	std::cout << "graph_alg_test_env v0.1.1.10 by Botond Ortutay \n";
+	std::cout << "graph_alg_test_env v0.2.0.0 by Botond Ortutay \n";
 	std::cout << "Please enter a command! \n";
 
 	if(loadModule("main", commDataStructure) == 1) { return 1; } // Loads the module and crashes if loading unsuccesful
