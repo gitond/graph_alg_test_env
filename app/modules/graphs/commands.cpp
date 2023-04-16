561af32840efd6f362777640414fbf3113a991a6
Botond Ortutay <boortu@utu.fi>
2023-04-16 15:58:53 +0300

    experimentation with stirng to number during command flag parsing

--- app/modules/graphs/commands.cpp
+++ app/modules/graphs/commands.cpp
@@ -3,6 +3,7 @@
 #include "../../metaheaders/commstruct.h"
 #include "commands.h"
 #include <iostream>
+#include <stdexcept>
 
 // Command executions
 namespace graphComms {
@@ -13,6 +14,14 @@ namespace graphComms {
 				if (flags[0] == "" || flags[1] == "" || flags[2] == "") {
 					return "Please use correct format: nvertex NAME X-VALUE Y-VALUE";
 				}
+
+				try {
+					std::stof(flags[1]);
+					std::stof(flags[2]);
+				} catch (const std::invalid_argument& ia) {
+					return "Please give me numerical x and y values";
+				}
+
 				std::cout << "New vertex " << flags[0] << " at x: " << flags[1] << ", y: " << flags[2];
 				return "";
 		}
