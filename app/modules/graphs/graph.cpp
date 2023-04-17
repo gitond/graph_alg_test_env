926848d784aa30d9d2091f5f896528edea51e464
Botond Ortutay <boortu@utu.fi>
2023-04-17 13:19:37 +0300

    Graph constructor now checks adjacency matrix validity

--- app/modules/graphs/graph.cpp
+++ app/modules/graphs/graph.cpp
@@ -4,11 +4,24 @@
 // This is the definition for the constructor of graph.
 // The class definition as well as the declarations are in the header "graph.h"
 graph::graph(std::array<std::array<float, 250>, 250> am, std::array<vertex,250> v2, int length){
-	adjMatrix = am;
-	vertices = v2;
-	len = length;
+	// Adjacency matrix validation
+	int validMatrix = 1;
+	for(int i = 0; i < length; i++){
+		if (am[i][i] != 0) { validMatrix = 0; }	// weights between v1,v1 should always be 0
+		for(int j = 0; j < length; j++){
+			if (am[i][j] != am[j][i]) { validMatrix = 0; } // weights between v1,v2 = v2,v1 always
+		}
+	}
 
-	std::cout << "Graph constructor called \n";
+	if (validMatrix) {
+		adjMatrix = am;
+		vertices = v2;
+		len = length;
+
+		std::cout << "Graph constructor successful \n";
+	} else {
+		std::cout << "Graph constructor failed! Invalid matrix.";
+	}
 }
 
 int main(){
@@ -16,6 +29,10 @@ int main(){
 
 	std::array<std::array<float, 250>, 250> am;
 	std::array<vertex,250> v2;
+
+	am[0][0] = 0;	am[0][1] = 1;	am[0][2] = 1;
+	am[1][0] = 1;	am[1][1] = 0;	am[1][2] = 0;
+	am[2][0] = 1;	am[2][1] = 0;	am[2][2] = 0;
 
 	v2[0] = vertex("A",1,4);
 	v2[1] = vertex("B",1,4);
