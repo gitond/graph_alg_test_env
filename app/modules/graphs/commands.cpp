72413eb01b45eddb0821c726159d655a2f62b943
Botond Ortutay <boortu@utu.fi>
2023-04-20 12:29:15 +0300

    connsto (neighbor) command added. integrated base graph module with cli

--- app/modules/graphs/commands.cpp
+++ app/modules/graphs/commands.cpp
@@ -12,6 +12,7 @@
 namespace graphComms {
 	std::string execute(int commandId, std::string flags[10], graph& g){
 		std::array<vertex,250> sgv;
+		std::array<vertex,100> na;
 		std::array<vertex,2> dva;
 		int ffi = -1;
 		std::array<std::array<float, 250>, 250> am;
@@ -94,6 +95,43 @@ namespace graphComms {
 				return "";
 			case 204: //glength
 				std::cout << "The current length of the graph is " << g.length();
+				return "";
+			case 205: //connsto
+				if (flags[0] == "") {
+					return "Please use correct format: conncto VERTEX_NAME";
+				}
+
+
+				sgv = g.getVertices();
+				for(int i = 0; i < g.length(); i++){
+					if (sgv[i].getName() == flags[0]){
+						dva[0] = sgv[i];
+						break;
+					}
+				}
+
+				if (dva[0].getName() == "") { return "ERROR: The vertex you entered doesn't exist in the graph."; }
+
+				na = g.neighbors(dva[0]);
+
+				if (na[0].getName() == "") {
+					/*
+						Means array of nullvertices returned from .neighbors
+						.neighbors already printed error message
+					*/
+					return "";
+				}
+
+				std::cout << "The neighbors of vertex " << dva[0].getName() << " are: ";
+
+				for(int i = 0; i < 100; i++){
+					if (na[i].getName() == "") {
+						break;
+					} else {
+						std::cout << na[i].getName() << " ";
+					}
+				}
+
 				return "";
 		}
 	}
