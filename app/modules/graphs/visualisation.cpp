86e81e289bf45c8e5d4595283462a29d5ab10caf
Botond Ortutay <boortu@utu.fi>
2023-04-21 11:43:53 +0300

    Drawing ines with sfml

--- app/modules/graphs/visualisation.cpp
+++ app/modules/graphs/visualisation.cpp
@@ -7,10 +7,21 @@ sf::CircleShape dot(int posX, int posY){
 	return dot;
 }
 
+sf::VertexArray line(int posX1, int posY1, int posX2, int posY2){
+	sf::VertexArray lines(sf::LinesStrip,2);
+	lines[0].position = sf::Vector2f(posX1+2,posY1+2);
+	lines[0].color = sf::Color::Green;
+	lines[1].position = sf::Vector2f(posX2+2,posY2+2);
+	lines[1].color = sf::Color::Green;
+	return lines;
+}
+
 int drawGraph(int dimensionX, int dimensionY){
 	sf::RenderWindow window(sf::VideoMode(dimensionX, dimensionY), "graph_alg_test_env_window");
 	sf::CircleShape dotOne = dot(200,40);
 	sf::CircleShape dotTwo = dot(160,80);
+
+	sf::VertexArray AB = line(200,40,160,80);
 
 	while (window.isOpen())
 	{
@@ -24,6 +35,7 @@ int drawGraph(int dimensionX, int dimensionY){
 		window.clear();
 		window.draw(dotOne);
 		window.draw(dotTwo);
+		window.draw(AB);
 		window.display();
 	}
 
