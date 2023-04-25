8bf59c0751ba5961a458fdd2bed920eca50bf561
Botond Ortutay <boortu@utu.fi>
2023-04-25 12:30:17 +0300

    vertex class updated according to issue #25

--- app/modules/graphs/vertex.cpp
+++ app/modules/graphs/vertex.cpp
@@ -2,30 +2,24 @@
 #include <iostream>
 #include "vertex.h"
 
-/*
-Vertex structure:
-private:
-	std::string name;
-	float posX;
-	float posY;
-public:
-	vertex(std::string cName, float cX, float cY){}	// constructor
-*/
-
-// This is the definition for the constructor of vertex.
-// The class definition as well as the declarations are in the header "vertex.h"
+// -- CONSTRUCTORS -- //
 vertex::vertex(std::string cName, float cX, float cY){
 	name = cName;
 	posX = cX;
 	posY = cY;
+	pathStatus = 0;
+	visitedStatus = 0;
 };
 
 vertex::vertex(){	// Constructor for "null" vertex for arrays
 	name = "";
 	posX = 0;
 	posY = 0;
+	pathStatus = 0;
+	visitedStatus = 0;
 }
 
+// -- GETTERS AND SETTERS -- //
 std::string vertex::getName(){
 	return name;
 }
@@ -36,4 +30,20 @@ float vertex::getPosX(){
 
 float vertex::getPosY(){
 	return posY;
+}
+
+int vertex::onPath(){
+	return pathStatus;
+}
+
+int vertex::isVisited(){
+	return visitedStatus;
+}
+
+void vertex::setPathStatus(int ps){
+	pathStatus = ps; // Not verified anywhere, but should always be 0 or 1
+}
+
+void vertex::setVisitedStatus(int vs){
+	visitedStatus = vs; // Not verified anywhere, but should always be 0 or 1
 }
--- app/modules/graphs/vertex.h
+++ app/modules/graphs/vertex.h
@@ -8,12 +8,21 @@ class vertex {
 		std::string name;
 		float posX;
 		float posY;
+		int pathStatus;
+		int visitedStatus;
 	public:
+		// Constructors
 		vertex(std::string cName, float cX, float cY);
 		vertex();
+
+		// Getters & Setters
 		std::string getName();
 		float getPosX();
 		float getPosY();
+		int onPath();		// Should return 1 (true) if on path and 0 (false) otherwise
+		int isVisited();	// Should return 1 (true) if visited and 0 (false) otherwise
+		void setPathStatus(int ps);
+		void setVisitedStatus(int vs);
 };
 
 #endif
